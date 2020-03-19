f6516bc869072d148f035812b24f7ce72d4bc4d8
Abebe Bediy <bediye@nordictrustee.com>
2020-03-19 10:19:08 +0100

    changed back to https support

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 namespace WeatherForecastAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("/")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
